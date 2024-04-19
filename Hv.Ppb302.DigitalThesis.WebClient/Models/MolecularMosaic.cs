a473dcdcaf75f49d38993646071c3cfb3f2d247b
haashim <haashem33@gmail.com>
2024-04-19 05:21:43 +0200

    Skapat databas samt intial modeller för våran databasstruktur

---
+++ Hv.Ppb302.DigitalThesis.WebClient/Models/MolecularMosaic.cs
@@ -0,0 +1,13 @@
+﻿namespace Hv.Ppb302.DigitalThesis.WebClient.Models
+{
+    public class MolecularMosaic
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public List<Tag> Tags { get; set; } = [];
+        public string? PdfFilePath { get; set; }
+        public string? AudioFilePath { get; set; }
+
+    }
+}
