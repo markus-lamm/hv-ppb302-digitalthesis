c4724659f86d0d9a6d336e1cabf69a85e6c0b354
markus-lamm <markus.lamm@outlook.com>
2024-04-26 18:43:42 +0200

    Added dynamic data in detail page

--- Hv.Ppb302.DigitalThesis.WebClient/Controllers/HomeController.cs
+++ Hv.Ppb302.DigitalThesis.WebClient/Controllers/HomeController.cs
@@ -9,11 +9,18 @@ namespace Hv.Ppb302.DigitalThesis.WebClient.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly GeoTagRepository _geoTagRepo;
+        private readonly MolarMosaicRepository _molarMosaicRepo;
+        private readonly MolecularMosaicRepository _molecularMosaicRepo;
 
-        public HomeController(ILogger<HomeController> logger, GeoTagRepository geoTagRepo)
+        public HomeController(ILogger<HomeController> logger, 
+            GeoTagRepository geoTagRepo, 
+            MolarMosaicRepository molarMosaicRepo, 
+            MolecularMosaicRepository molecularMosaicRepo)
         {
             _logger = logger;
             _geoTagRepo = geoTagRepo;
+            _molarMosaicRepo = molarMosaicRepo;
+            _molecularMosaicRepo = molecularMosaicRepo;
         }
 
         public IActionResult Index()
@@ -29,8 +36,40 @@ namespace Hv.Ppb302.DigitalThesis.WebClient.Controllers
         [Route("Home/Detail/{objectId}")]
         public IActionResult Detail(string objectId)
         {
-            ViewBag.ObjectId = objectId;
-            return View();
+            Guid guid = Guid.Parse(objectId);
+            var geoTag = _geoTagRepo.Get(guid);
+            var molarMosaics = _molarMosaicRepo.Get(guid);
+            var molecularMosaics = _molecularMosaicRepo.Get(guid);
+
+            static DetailViewModel BuildViewModel(dynamic model)
+            {
+                return new DetailViewModel
+                {
+                    ObjectId = model.Id,
+                    Title = model.Title,
+                    Content = model.Content,
+                    PdfFilePath = model.PdfFilePath,
+                    HasAudio = model.HasAudio,
+                    AudioFilePath = model.AudioFilePath,
+                };
+            }
+
+            if (geoTag != null)
+            {
+                return View(BuildViewModel(geoTag));
+            }
+            else if (molarMosaics != null)
+            {
+                return View(BuildViewModel(molarMosaics));
+            }
+            else if (molecularMosaics != null)
+            {
+                return View(BuildViewModel(molecularMosaics));
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         public IActionResult MolarMosaics()
---
+++ Hv.Ppb302.DigitalThesis.WebClient/Models/DetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Hv.Ppb302.DigitalThesis.WebClient.Models;
+
+public class DetailViewModel
+{
+    public Guid? ObjectId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public string? PdfFilePath { get; set; }
+    public bool? HasAudio { get; set; }
+    public string? AudioFilePath { get; set; }
+}
