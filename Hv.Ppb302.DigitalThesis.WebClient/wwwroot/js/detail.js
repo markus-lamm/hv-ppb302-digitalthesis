766bd20387032ccbcb8a9d39afaf745dd0c003a9
markus-lamm <markus.lamm@outlook.com>
2024-05-26 11:13:26 +0200

    Changes and bugfixes

--- Hv.Ppb302.DigitalThesis.WebClient/wwwroot/js/detail.js
+++ Hv.Ppb302.DigitalThesis.WebClient/wwwroot/js/detail.js
@@ -1,19 +1,20 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     const audioPlayer = document.getElementById('audio-player');
+    const audioImg = document.getElementById('audio-img');
+
     audioPlayer.addEventListener('click', function (event) {
         event.preventDefault();
-        const audioImg = document.getElementById('audio-img');
-        console.log("test");
-
         if (audioPlayer.paused) {
             audioPlayer.play();
             audioImg.src = '/images/icons/stop_icon.png';
-            console.log(audioImg.src);
         } else {
             audioPlayer.pause();
             audioPlayer.currentTime = 0;
             audioImg.src = '/images/icons/play_icon.png';
-            console.log(audioImg.src);
         }
+    });
+
+    audioPlayer.addEventListener('ended', function () {
+        audioImg.src = '/images/icons/play_icon.png';
     });
 });
