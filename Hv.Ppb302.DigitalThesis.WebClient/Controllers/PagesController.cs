020ea59c9fd224dbd7b67a6f103d6f6bab97cb36
markus-lamm <markus.lamm@outlook.com>
2024-08-30 09:47:18 +0200

    Added new page crud

---
+++ Hv.Ppb302.DigitalThesis.WebClient/Controllers/PagesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hv.Ppb302.DigitalThesis.WebClient.Data;
+using Hv.Ppb302.DigitalThesis.WebClient.Models;
+
+namespace Hv.Ppb302.DigitalThesis.WebClient.Controllers;
+
+public class PagesController : Controller
+{
+    private readonly PageRepository _pageRepo;
+
+    public PagesController(PageRepository pageRepo)
+    {
+        _pageRepo = pageRepo;
+    }
+
+    public IActionResult Index()
+    {
+        if (!CheckAuthentication())
+        {
+            return RedirectToAction("Login", "Admin");
+        }
+        return View(_pageRepo.GetAll());
+    }
+
+    public IActionResult Edit(Guid id)
+    {
+        if (!CheckAuthentication())
+        {
+            return RedirectToAction("Login", "Admin");
+        }
+
+        var page = _pageRepo.Get(id);
+        if (page == null)
+        {
+            return NotFound();
+        }
+
+        return View(page);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Guid id, [Bind("Id,Name,Content")] Page page)
+    {
+        if (!CheckAuthentication())
+        {
+            return RedirectToAction("Login", "Admin");
+        }
+
+        if (id != page.Id)
+        {
+            return NotFound();
+        }
+        if (!ModelState.IsValid)
+        {
+            return View(page);
+        }
+        _pageRepo.Update(page);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public bool CheckAuthentication()
+    {
+        return HttpContext.Session.GetString("Username") != null;
+    }
+}
