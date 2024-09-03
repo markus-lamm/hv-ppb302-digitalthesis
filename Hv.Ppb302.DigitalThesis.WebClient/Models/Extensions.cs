181e838bd9ff3ac9f12fbb04a73f16dc02a3eac1
Haashim Mohamed <hamo0021@hv.se>
2024-09-03 13:57:39 +0200

    Molarmosaic refactor

--- Hv.Ppb302.DigitalThesis.WebClient/Models/Extensions.cs
+++ Hv.Ppb302.DigitalThesis.WebClient/Models/Extensions.cs
@@ -1,23 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Hv.Ppb302.DigitalThesis.WebClient.Models
 {
     public static class Extensions
     {
-        public static IEnumerable<SelectListItem> ToSelectListItems<T>(
+
+        public static IEnumerable<SelectListItem> ToSelectListItemsList<T>(
             this IEnumerable<T>? items,
             Func<T, string> valueSelector,
             Func<T, string?> textSelector,
-            string? selectedValue = null)
+            IEnumerable<string>? selectedValues = null)
         {
-            // Use null-coalescing operator to handle null items
+            var selectedValuesSet = selectedValues?.ToHashSet() ?? new HashSet<string>();
+
             return (items ?? Enumerable.Empty<T>()).Select(item => new SelectListItem
             {
                 Value = valueSelector(item),
-                Text = textSelector(item),
-                Selected = selectedValue != null && valueSelector(item) == selectedValue
+                Text = textSelector(item) ?? string.Empty,
+                Selected = selectedValuesSet.Contains(valueSelector(item))
             }).ToList();
         }
+
+        public static List<string> ToStringListFromTagifyFormat(this string? json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return [];
+            }
+
+            var valueContainers = JsonSerializer.Deserialize<List<ValueContainer>>(json);
+            return valueContainers?.Where(vc => vc.Value != null)
+                       .Select(vc => vc.Value!)
+                       .ToList()
+                   ?? [];
+        }
+
+
 
     }
 }
--- Hv.Ppb302.DigitalThesis.WebClient/Models/MolarMosaicCrudViewModel.cs
+++ Hv.Ppb302.DigitalThesis.WebClient/Models/MolarMosaicCrudViewModel.cs
@@ -1,19 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Hv.Ppb302.DigitalThesis.WebClient.Models
 {
     public class MolarMosaicCrudViewModel
     {
-        public MolecularMosaic? MolarMosaic { get; set; }
+        public MolarMosaic? MolarMosaic { get; set; } 
         public IEnumerable<SelectListItem>? ConnectorTags { get; set; } = Enumerable.Empty<SelectListItem>();
         public IEnumerable<SelectListItem>? AssemblageTags { get; set; } = Enumerable.Empty<SelectListItem>();
         public IEnumerable<SelectListItem>? KaleidoscopeTags { get; set; } = Enumerable.Empty<SelectListItem>();
-        public List<string> Becomings { get; set; } = new List<string>();
-        
-        public List<Guid>? SelectedConnectorsTagsIds { get; set; } = [];
-        public List<Guid>? SelectedKaleidoscopeTagsIds { get; set; } = [];
-        public Guid SelectedAssemblageId { get; set; }
-        public List<string>? SelectedBecomings { get; set; } = [];
+        public string? Becomings { get; set; }
+        public List<Guid> SelectedConnectorsTagsIds { get; set; } = [];
+        public List<Guid> SelectedKaleidoscopeTagsIds { get; set; } = [];
 
     }
 }
