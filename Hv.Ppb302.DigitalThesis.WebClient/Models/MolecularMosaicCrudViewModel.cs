702ec394be5b78b1356e3fc3b532f6e2e331c1ae
Markus Lamm <mala0167@hv.se>
2024-09-05 13:27:35 +0200

    Improved profile credentials change

--- Hv.Ppb302.DigitalThesis.WebClient/Models/MolecularMosaicCrudViewModel.cs
+++ Hv.Ppb302.DigitalThesis.WebClient/Models/MolecularMosaicCrudViewModel.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Hv.Ppb302.DigitalThesis.WebClient.Models
+namespace Hv.Ppb302.DigitalThesis.WebClient.Models;
+
+public class MolecularMosaicCrudViewModel
 {
-    public class MolecularMosaicCrudViewModel
-    {
-        public MolecularMosaic? MolecularMosaic { get; set; }
-        public IEnumerable<SelectListItem>? ConnectorTagsItemList { get; set; } = Enumerable.Empty<SelectListItem>();
-        public IEnumerable<SelectListItem>? AssemblageTagsItemList { get; set; } = Enumerable.Empty<SelectListItem>();
-        public IEnumerable<SelectListItem>? KaleidoscopeTagsItemList { get; set; } = Enumerable.Empty<SelectListItem>();
-        public string? Becomings { get; set; }
-        public List<Guid> SelectedConnectorsTagsIds { get; set; } = [];
-        public List<Guid> SelectedKaleidoscopeTagsIds { get; set; } = [];
-    }
+    public MolecularMosaic? MolecularMosaic { get; set; }
+    public IEnumerable<SelectListItem>? ConnectorTagsItemList { get; set; } = Enumerable.Empty<SelectListItem>();
+    public IEnumerable<SelectListItem>? AssemblageTagsItemList { get; set; } = Enumerable.Empty<SelectListItem>();
+    public IEnumerable<SelectListItem>? KaleidoscopeTagsItemList { get; set; } = Enumerable.Empty<SelectListItem>();
+    public string? Becomings { get; set; }
+    public List<Guid> SelectedConnectorsTagsIds { get; set; } = [];
+    public List<Guid> SelectedKaleidoscopeTagsIds { get; set; } = [];
 }
