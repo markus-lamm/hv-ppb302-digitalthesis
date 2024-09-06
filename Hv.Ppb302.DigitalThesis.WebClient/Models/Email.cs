09476694774bf5dbfd013466eecd5ffe5bf7dedb
Haashim Mohamed <hamo0021@hv.se>
2024-09-06 15:37:14 +0200

    Mail service added

---
+++ Hv.Ppb302.DigitalThesis.WebClient/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿namespace Hv.Ppb302.DigitalThesis.WebClient.Models
+{
+    public class Email
+    {
+        public string Receiver { get; set; }
+        public string Subject { get; } = "Avhandlinglänk";
+        public string Body { get; } = "Här kommer din nya hemasidan";
+
+        public Email(string receiver)
+        {
+            Receiver = receiver;
+        }
+
+    }
+}
---
+++ Hv.Ppb302.DigitalThesis.WebClient/Services/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using Hv.Ppb302.DigitalThesis.WebClient.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace Hv.Ppb302.DigitalThesis.WebClient.Services
+{
+    public class EmailService
+    {
+        private readonly SmtpClient _smtpClient = new()
+        {
+            Host = "smtp-mail.outlook.com",
+            Port = 587,
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential("DigitalAvhandlingen@outlook.com", "H0gskolanVastDigitalAvhandling")
+        };
+
+        private readonly MailMessage _mailMessage;
+
+        public EmailService(Email email)
+        {
+            using var message = new MailMessage("DigitalAvhandlingen@outlook.com", email.Receiver);
+            message.Subject = email.Subject;
+            message.Body = email.Body;
+
+            _mailMessage = message;
+        }
+
+        public void SendMail()
+        { 
+            _smtpClient.Send(_mailMessage);
+        }
+    }
+}
